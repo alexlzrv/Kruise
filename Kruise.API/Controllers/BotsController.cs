dc42d13b1a91ef4b803678fbe0f4d12ccc39e356
mayr0y <alexlzrw@gmail.com>
2022-07-09 18:52:08 +0300

    wip: added telegram bot

---
+++ Kruise.API/Controllers/BotsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+
+namespace Kruise.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BotsController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Post([FromServices] HandleUpdateService handleUpdateService,
+                                          [FromBody] Update update)
+    {
+        await handleUpdateService.EchoAsync(update);
+        return Ok();
+    }
+}
