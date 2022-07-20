7af9a9edc7ad20ba6fbc6dd8f57b36463e659840
mayr0y <alexlzrw@gmail.com>
2022-07-20 21:24:15 +0300

    feat: added auth

---
+++ Kruise.API/Contracts/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kruise.API.Contracts;
+
+public class LoginRequest
+{
+    [EmailAddress]
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    [Compare(nameof(Password))]
+    public string ConfirmPassword { get; set; }
+}
